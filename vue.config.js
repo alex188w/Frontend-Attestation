0f9eedc82c1cdb1ef0deb760c1f8d5288d250c55
alex188w <alex18w@mail.ru>
2024-03-21 21:52:08 +0500

    deploy1

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  // transpileDependencies: true
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Frontend-Attestation/'
-    : '/'
-})
-
 // const { defineConfig } = require('@vue/cli-service')
 // module.exports = defineConfig({
-//   transpileDependencies: true
+//   // transpileDependencies: true
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/Frontend-Attestation/'
+//     : '/'
 // })
+
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
